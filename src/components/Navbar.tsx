e93bd72af12f613658e74933fded738a217fb872
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-12-09 21:03:04 +0330

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import "./navbar.css";
+
+interface IProps {}
+
+const Navbar: React.FC<IProps> = (props) => {
+  return (
+    <Router>
+      <div className="navbar__wrapper">
+        <NavLink to="/">
+          <h2>Single SPA (microfrontend)</h2>
+        </NavLink>
+        <div className="flex-between">
+          <NavLink to="/contact">contact</NavLink>
+          <NavLink to="/about">about</NavLink>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default Navbar;
